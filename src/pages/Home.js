10331785f47d159b4947626abacbeb1413428bd3
rodrigojmayer <rodrigojmayer>
2023-03-02 10:40:36 +0100

    test cors change in the backend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import axios from 'axios';
 
 // components
 import WorkoutDetails from '../components/WorkoutDetails'
@@ -7,10 +8,14 @@ import WorkoutForm from '../components/WorkoutForm'
 
 const Home = () => {
     const {workouts, dispatch} = useWorkoutsContext()
+    
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch('/api/workouts')
+            // console.log(  "process.env.REACT_APP_PROXY_HOST 1 " + process.env.REACT_APP_PROXY_HOST)
+            // const response = await fetch('/api/workouts')
+            const response = await fetch('https://mern-task-app-api-nvn3.onrender.com/api/workouts')
+            // const response = await axios('https://mern-task-app-api-nvn3.onrender.com/api/workouts')
             const json = await response.json()
 
             if (response.ok) {
@@ -21,8 +26,8 @@ const Home = () => {
         fetchWorkouts()
     }, [dispatch])
 
-    console.log(  "process.env.REACT_APP_PROXY_HOST " + process.env.REACT_APP_PROXY_HOST)
-    console.log(  "test " + process.env.TEST)
+    // console.log(  "process.env.REACT_APP_PROXY_HOST " + process.env.REACT_APP_PROXY_HOST)
+    // console.log(  "test " + process.env.TEST)
 
     return (
         <div className="home">
